8a1484f2a5386f1bfff8ba86248a15d59dda340c
Armin Ribic <arminribic@web.de>
2025-10-08 10:47:31 +0200

    SCRUM-20 EmployeeDto und EmployeeService implementiert

---
+++ src/main/java/de/szut/lf8_starter/employee/EmployeeService.java
@@ -0,0 +1,35 @@
+package de.szut.lf8_starter.employee;
+
+import de.szut.lf8_starter.employee.dto.EmployeeDto;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class EmployeeService {
+
+
+    private final RestTemplate restTemplate;
+    private String baseUrl = "https://employee-api.szut.dev/";
+
+    public EmployeeService(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
+    public EmployeeDto getById(Long employeeId) {
+        try {
+            return restTemplate.getForObject(
+                    baseUrl + "/employees/{employeeId}",
+                    EmployeeDto.class,
+                    employeeId
+            );
+        } catch (HttpClientErrorException.NotFound e) {
+            return null;
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+
+}
+
---
+++ src/main/java/de/szut/lf8_starter/employee/dto/EmployeeDto.java
@@ -0,0 +1,15 @@
+package de.szut.lf8_starter.employee.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class EmployeeDto {
+
+    private Long id;
+    private List<String> qualifications;
+
+}
