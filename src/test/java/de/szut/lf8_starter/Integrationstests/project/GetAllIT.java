64713e69452f71c654d44ad1e2826091bee57417
y.Hudylko <y.hudylko@neusta.de>
2025-10-17 15:23:13 +0200

    SCRUM-62 Integrationstests angefangen

---
+++ src/test/java/de/szut/lf8_starter/Integrationstests/project/GetAllIT.java
@@ -0,0 +1,43 @@
+package de.szut.lf8_starter.Integrationstests.project;
+
+import de.szut.lf8_starter.project.ProjectEntity;
+import de.szut.lf8_starter.testcontainers.AbstractIntegrationTest;
+import org.junit.jupiter.api.Test;
+import org.springframework.security.test.context.support.WithMockUser;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+
+
+public class GetAllIT  extends AbstractIntegrationTest {
+
+    @Test
+    @WithMockUser(roles = "user")
+    void findAllProjects() throws Exception {
+
+        ProjectEntity project1 = new ProjectEntity();
+        project1.setName("Test Project 1");
+        project1.setResponsibleEmployeeId(1L);
+        project1.setCustomerId(1L);
+        project1.setResponsibleCustomerName("Jan");
+        projectRepository.save(project1);
+
+        ProjectEntity project2 = new ProjectEntity();
+        project2.setName("Test Project 2");
+        project2.setResponsibleEmployeeId(2L);
+        project2.setCustomerId(2L);
+        project2.setResponsibleCustomerName("Yana");
+        projectRepository.save(project2);
+
+        this.mockMvc.perform(get("/projects")
+                .with(csrf()))
+                .andExpect(status().is2xxSuccessful())
+                .andExpect(jsonPath("$", hasSize(2)))
+                .andExpect(jsonPath("$[0].message", is("Test Project 1")))
+                .andExpect(jsonPath("$[1].message", is("Test Project 2")));
+    }
+}
