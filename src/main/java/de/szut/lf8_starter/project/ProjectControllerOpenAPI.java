3b19a539651d6d9f554c9ccb2b1db96f602d8061
janfolkerts <jfolkerts@neusta.de>
2025-10-17 11:04:49 +0200

    SCRUM-36 Swagger Dokumentation

--- src/main/java/de/szut/lf8_starter/project/ProjectControllerOpenAPI.java
+++ src/main/java/de/szut/lf8_starter/project/ProjectControllerOpenAPI.java
@@ -10,14 +10,30 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 
 public interface ProjectControllerOpenAPI {
 
-    @Operation(summary = "creates a new project with its id and project information")
+    @Operation(
+            summary = "creates a new project with its id and project information")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "201", description = "created project",
                     content = {@Content(mediaType = "application/json",
-                            schema = @Schema(implementation = ProjectGetDto.class))}),
-            @ApiResponse(responseCode = "400", description = "invalid JSON posted",
+                            schema = @Schema(
+                                    implementation = ProjectGetDto.class))}),
+            @ApiResponse(responseCode = "400",
+                    description = "invalid JSON posted",
                     content = @Content),
             @ApiResponse(responseCode = "401", description = "not authorized",
                     content = @Content)})
     ProjectGetDto create(ProjectCreateDto projectCreateDto);
+
+    @Operation(summary = "deletes an existing project by id " +
+            "information")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "delete " +
+                    "successful"),
+            @ApiResponse(responseCode = "401", description = "not authorized" +
+                    "posted",
+                    content = @Content),
+            @ApiResponse(responseCode = "404", description = "resource not " +
+                    "found",
+                    content = @Content)})
+    void deleteProjectById(long id);
 }
