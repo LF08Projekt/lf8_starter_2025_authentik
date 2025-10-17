0baad6adfe11e9eef6338afeae4920e551746b22
y.Hudylko <y.hudylko@neusta.de>
2025-10-17 14:27:49 +0200

    SCRUM-60 Swagger Dokumentation

--- src/main/java/de/szut/lf8_starter/project/ProjectControllerOpenAPI.java
+++ src/main/java/de/szut/lf8_starter/project/ProjectControllerOpenAPI.java
@@ -2,6 +2,7 @@ package de.szut.lf8_starter.project;
 
 import de.szut.lf8_starter.project.dto.ProjectCreateDto;
 import de.szut.lf8_starter.project.dto.ProjectGetDto;
+import de.szut.lf8_starter.project.dto.ProjectUpdateDto;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
@@ -24,6 +25,7 @@ public interface ProjectControllerOpenAPI {
                     content = @Content)})
     ProjectGetDto create(ProjectCreateDto projectCreateDto);
 
+
     @Operation(summary = "deletes an existing project by id " +
             "information")
     @ApiResponses(value = {
@@ -36,4 +38,19 @@ public interface ProjectControllerOpenAPI {
                     "found",
                     content = @Content)})
     void deleteProjectById(long id);
+
+
+    @Operation(summary = "updates a project by id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "project updated",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ProjectUpdateDto.class))}),
+            @ApiResponse(responseCode = "404", description = "project not found",
+                    content = @Content),
+            @ApiResponse(responseCode = "400", description = "invalid JSON posted",
+                    content = @Content),
+            @ApiResponse(responseCode = "401", description = "not authorized",
+                    content = @Content)})
+    ProjectGetDto updateProject(long id, ProjectUpdateDto projectUpdateDto);
+
 }
